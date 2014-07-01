ab7a5c1678424c3a151f84b57816d9957c32e8bd
jdodds <jdodds@storm.dev.galois.com>
2014-07-01 09:57:38 -0700

    Changes for portability
    added .h file with prototypes for sha functions that are called
    got rid of a dependence on a libsodium variable
    added local directory for makefile so libsodium.a can be added there

---
+++ callsha/hashes.h
@@ -0,0 +1,3 @@
+int crypto_hash_sha256(unsigned char *out, unsigned char *in, unsigned long long inlen);
+void SHA256(unsigned char *in, unsigned long inlen, unsigned char *out);
+int SHA256_HashBuf(unsigned char *out, unsigned char *in, unsigned int inlen );
