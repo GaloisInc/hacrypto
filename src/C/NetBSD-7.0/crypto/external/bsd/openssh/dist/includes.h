e4e97b6f8713938d76b154e59a03b5e51f24a0aa
Joseph Kiniry <kiniry@galois.com>
2015-11-05 09:22:50 -0800

    Snapshot of crypto libraries in NetBSD 7.0.

---
+++ src/C/NetBSD-7.0/crypto/external/bsd/openssh/dist/includes.h
@@ -0,0 +1,9 @@
+/*	$NetBSD: includes.h,v 1.2.22.1 2015/04/30 06:07:30 riz Exp $	*/
+#include <sys/cdefs.h>
+#ifndef __OpenBSD__
+#define __bounded__(a, b, c)
+#define explicit_bzero(a, b) explicit_memset((a), 0, (b))
+#define timingsafe_bcmp(a, b, c) (!consttime_memequal((a), (b), (c)))
+#endif
+
+#include "namespace.h"
---
+++ src/C/NetBSD-7.0/crypto/external/bsd/openssh/dist/version.h
@@ -0,0 +1,13 @@
+/*	$NetBSD: version.h,v 1.12.4.1 2015/04/30 06:07:31 riz Exp $	*/
+/* $OpenBSD: version.h,v 1.72 2015/03/04 18:53:53 djm Exp $ */
+
+#define __OPENSSH_VERSION	"OpenSSH_6.8"
+#define __NETBSDSSH_VERSION	"NetBSD_Secure_Shell-20150403"
+#define SSH_HPN         "-hpn13v14"
+#define SSH_LPK		"-lpk"
+/*
+ * it is important to retain OpenSSH version identification part, it is
+ * used for bug compatibility operation.  present NetBSD SSH version as comment
+ */
+#define SSH_VERSION	__OPENSSH_VERSION " " __NETBSDSSH_VERSION
+#define SSH_RELEASE SSH_VERSION SSH_HPN SSH_LPK
